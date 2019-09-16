8872a396eef9bbeec6b1351f5028a739951c516b
Min Yoon <stdev17@gmail.com>
2019-09-16 09:01:07 +0900

    Updates

---
+++ precache-manifest.3c6527b3580f12f32b2b661af60f5248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dd3221c6bca940d59d975fa80aa9ae",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "1fe37da724ed9fc5b273",
+    "url": "/cv/static/css/main.8f63a153.chunk.css"
+  },
+  {
+    "revision": "59aef5538fead59b8669",
+    "url": "/cv/static/js/2.29280262.chunk.js"
+  },
+  {
+    "revision": "1fe37da724ed9fc5b273",
+    "url": "/cv/static/js/main.bffabcb7.chunk.js"
+  },
+  {
+    "revision": "ec79c9554791a3dec290",
+    "url": "/cv/static/js/runtime~main.0d5561da.js"
+  }
+]);
